75e3cfda0adf1fd5993801a86968abd11d9faee2
Punit Kumar <tagpunitkumar@gmail.com>
2023-10-23 22:48:22 +0530

    Navbar

---
+++ nextclient/app/home/page.jsx
@@ -0,0 +1,18 @@
+export default function Page() {
+  return (
+    <>
+    <div className="bg-gradient-to-r from-blue-400 to-purple-500 via-green-500">
+      <div className="flex flex-row justify-between">
+        <h1 className="m-2 font-bold inter">NyaySarthi</h1>
+        <ul className="flex flex-row justify-between items-center w-1/2 m-2 font-semibold">
+          <li>Home</li>
+          <li>Lawyers</li>
+          <li>Marketplace</li>
+          <li>Profile</li>
+          <li>About</li>
+        </ul>
+      </div>
+    </div>
+    </>
+  );
+}
